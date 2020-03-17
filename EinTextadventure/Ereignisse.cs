6e867e4a1a70dfebeeaf30b56bae945e4c41bd2b
HenningBB <61740986+HenningBB@users.noreply.github.com>
2020-03-17 15:25:26 +0100

    Klasse erstellt in der der Ablauf geschieht

---
+++ EinTextadventure/Ereignisse.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EinTextadventure
+{
+    public class Ereignisse
+    {
+        string weg;
+        public Ereignisse()
+        {
+        }
+
+        public void start()
+        {
+            Console.WriteLine("Herzlich Willkommen zum Wald-Spaziergang!");
+            Console.WriteLine("Vor sich sehen sie zwei Wege.");
+            Console.WriteLine("Das Schild das an der Abzweigung steht besagt das einer länger als der andere ist.");
+            Console.WriteLine("Wähle nun den Weg den du gehen willst:");
+           
+        }
+
+        public void punkt1()
+        {
+            
+            Console.WriteLine("Den langen Weg(1) oder den kurzen Weg(2)[Bitte gebe die Zahl des gewählten Weges ein!]");
+            weg = Console.ReadKey().KeyChar.ToString();
+            Console.WriteLine();
+            switch (weg)
+            {
+                case "1":
+                    //weg 1
+                    Console.WriteLine("Langer Weg");
+                    break;
+                case "2":
+                    //weg 2
+                    Console.WriteLine("Kurzer Weg");
+                    break;
+                default:
+                    Console.WriteLine("Falsche Eingabe!");
+                    punkt1();
+                    break;
+            }
+        }
+    }
+}
--- EinTextadventure/Program.cs
+++ EinTextadventure/Program.cs
@@ -4,30 +4,12 @@ namespace EinTextadventure
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            string antwort;
-            Console.WriteLine("Herzlich Willkommen zum Wald-Spaziergang!");
-            Console.WriteLine("Vor sich sehen sie zwei Wege.");
-            Console.WriteLine("Das Schild das an der Abzweigung steht besagt das einer länger als der andere ist.");
-            Console.WriteLine("Wähle nun den Weg den du gehen willst:");
-            Console.WriteLine("Den langen Weg(1) oder den kurzen Weg(2)[Bitte gebe die Zahl des gewählten Weges ein!]");
-            antwort = Console.ReadKey().KeyChar.ToString();
-            Console.WriteLine();
-            switch (antwort)
-            {
-                case "1":
-                    //weg 1
-                    Console.WriteLine("Langer Weg");
-                    break;
-                case "2":
-                    //weg 2
-                    Console.WriteLine("Kurzer Weg");
-                    break;
-                default:
-
-                    break;
-            }
+            Ereignisse ereignisse = new Ereignisse();
+            ereignisse.start();
+            ereignisse.punkt1();         
             Console.ReadLine();
         }
     }
