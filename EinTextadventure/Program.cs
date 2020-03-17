358b5e979371405e865a61a053b63b55b1885346
HenningBB <61740986+HenningBB@users.noreply.github.com>
2020-03-17 11:13:48 +0100

    einfache Initialisierung, Ablauf muss noch in Klassen überführt werden

--- EinTextadventure/Program.cs
+++ EinTextadventure/Program.cs
@@ -6,7 +6,29 @@ namespace EinTextadventure
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string antwort;
+            Console.WriteLine("Herzlich Willkommen zum Wald-Spaziergang!");
+            Console.WriteLine("Vor sich sehen sie zwei Wege.");
+            Console.WriteLine("Das Schild das an der Abzweigung steht besagt das einer länger als der andere ist.");
+            Console.WriteLine("Wähle nun den Weg den du gehen willst:");
+            Console.WriteLine("Den langen Weg(1) oder den kurzen Weg(2)[Bitte gebe die Zahl des gewählten Weges ein!]");
+            antwort = Console.ReadKey().KeyChar.ToString();
+            Console.WriteLine();
+            switch (antwort)
+            {
+                case "1":
+                    //weg 1
+                    Console.WriteLine("Langer Weg");
+                    break;
+                case "2":
+                    //weg 2
+                    Console.WriteLine("Kurzer Weg");
+                    break;
+                default:
+
+                    break;
+            }
+            Console.ReadLine();
         }
     }
 }
