25dd8d668065a7c09bbf094b7472ecc7354a2a8a
HenningBB <61740986+HenningBB@users.noreply.github.com>
2020-03-24 14:32:59 +0100

    interfaces erstellt und Dokumetation in der StarUML-Datei erweitert

---
+++ EinTextadventure/IErzaehler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EinTextadventure
+{
+    public interface IErzaehler
+    {
+       ISzene aktuelleSzene{get;set;}
+    }
+}
---
+++ EinTextadventure/ISzene.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EinTextadventure
+{
+    public interface ISzene
+    {
+        int ID { get; set; }
+        int Ergebnis { get; set; }
+        string Inhalt { get; set; }
+    }
+}
