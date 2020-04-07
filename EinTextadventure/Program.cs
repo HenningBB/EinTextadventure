15b220012e6b03726f87caf93f405fea9a005b20
HenningBB <61740986+HenningBB@users.noreply.github.com>
2020-04-07 11:02:06 +0200

    Aufräumen

--- EinTextadventure/Program.cs
+++ EinTextadventure/Program.cs
@@ -7,14 +7,21 @@ namespace EinTextadventure
 
         static void Main(string[] args)
         {
-            Erzaehler erzaehler = new Erzaehler();
-            bool enden = false;
-            while (!enden)
-            {
-                Console.Write(erzaehler.spieleSzene()+"\n");
-                erzaehler.ladeErgebnis(Convert.ToInt32(Console.ReadKey().KeyChar.ToString()));
-                enden = true;
-            }
+            Console.WriteLine("Herzlich Willkommen zum Wald-Spaziergang!");
+            Console.WriteLine("Vor dir siehst du zwei Wege. Das Schild das an der Abzweigung steht besagt das einer länger als der andere ist.");
+            Console.WriteLine("Wähle nun den Weg den du gehen willst:");
+            Console.WriteLine("Den langen Weg(1) oder den kurzen Weg(2)[Bitte gebe die Zahl des gewählten Weges ein!]");
+            Console.WriteLine("Du gehst den Weg entlang.");
+            Console.WriteLine("Ab und zu blendet dich die Sonne, wenn sie nicht durch die Baumwipfel geblockt wird.");
+            Console.WriteLine("In der Ferne zwitschern die Vögel.");
+            Console.WriteLine("Auf einmal steht ein Hund vor dir.");
+            Console.WriteLine("Er schaut dich an und wackelt mit seinem Schweif.");
+            Console.WriteLine("Bevor du reagieren kannst rennt er in den Wald.");
+            Console.WriteLine("Du siehst wie er kurz nach den ersten Bäumen nicht mehr zu sehen ist.");
+            Console.WriteLine("Bei näherer Betrachtung scheint er wie vom Erdboden verschluckt.");
+            Console.WriteLine("Den langen Weg(1) oder den kurzen Weg(2)[Bitte gebe die Zahl des gewählten Weges ein!]");
+            Console.WriteLine("Schaust du nun nach wo er abgebllieben ist[1] oder gehst du weiter spazieren[2]?.");
+            Console.Read();
         }
     }
 }
